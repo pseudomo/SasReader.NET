bd4a6102dce983b30ac513de93c54910157c34af
pseudomo <pseudomo@yandex.ru>
2022-07-16 20:02:28 +0200

    deleted unused file

--- src/date/NotImplementedException.cs
+++
@@ -1,9 +0,0 @@
-package com.epam.parso.date;
-
-/**
- * Declared but not yet implemented formats throw this type of exception.
- * This exception is suppose to be caught internally to substitute
- * not implemented with on of fallback date format.
- */
-class NotImplementedException extends RuntimeException {
-}
